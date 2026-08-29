db582f8cf5a6faeb7bdeff3b78c6b8c17650abbb
vishvam839 <vishvam1238@gmail.com>
2026-08-29 17:01:56 +0530

    Time: 0 ms (100%), Space: 10.1 MB (44.59%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.c
@@ -0,0 +1,26 @@
+int search(int* nums, int numsSize, int target) {
+
+    int left = 0;
+    int right = numsSize - 1;
+    int mid;
+
+    while (left <= right) 
+    {
+        mid = (left + right) / 2;
+
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+
+        if (nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
